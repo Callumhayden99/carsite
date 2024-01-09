8118146c861244bfcd8870090e915c949fdf7128
callumhayden <callum.hayden@gmail.com>
2024-01-09 19:11:42 +0000

    got carousel working and just playing round with imgages and style

--- my-react-app/src/components/Carousel.jsx
+++ my-react-app/src/components/Carousel.jsx
@@ -1,23 +1,32 @@
-
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
+import lambo from '../assets/lambo.jpg';
+import aston from '../assets/aston.jpg'
+import r8 from '../assets/r8.jpg'
 
-export const Carousel = () => {
+const Carousel = () => {
   return (
     <CarouselProvider
-     naturalSlideWidth={100}
-     naturalSlideHeight={120}
-     totalSlides={3}
+      naturalSlideWidth={200}
+      naturalSlideHeight={120}
+      totalSlides={3}
     >
+      <Slider> 
+        <Slide index={0}> Lamborghini Huracan Tecnica
+          <img src={lambo} width={250} alt="Lamborghini Huracan Tecnica" />
+        </Slide>
+        <Slide index={1}>Aston Martin Vantage
+        <img src={aston} width={250} alt="Aston Martin Vantage"></img>
+        </Slide>
+        <Slide index={2}>Audi R8 Spyder
+        <img src={r8} width={250} alt="Audi R8 Spyder"></img>
+         </Slide>
+      </Slider>
 
-    <Slider>
-      <Slide index={0}>Slide 1</Slide>
-      <Slide index={1}>Slide 2</Slide>
-      <Slide index={2}>Slide 3</Slide>
-    </Slider>
-
-    <ButtonBack>Back</ButtonBack>
-    <ButtonNext>Next</ButtonNext>
-   </CarouselProvider>
+      <ButtonBack>Back</ButtonBack>
+      <ButtonNext>Next</ButtonNext>
+    </CarouselProvider>
   );
 };
+
+export default Carousel;
