b73e809a7d91b6102dac9f0d8a809a80e0dc3d0f
callumhayden <callum.hayden@gmail.com>
2024-01-21 20:03:07 +0000

    added the remainder of the images that i needed, also got the new files set up for the backend

---
+++ my-react-app/src/utils/server.js
@@ -0,0 +1,18 @@
+//Include the express library
+import express, { json } from "express";
+//Include the morgan middleware
+import morgan from "morgan";
+//Include the cors middleware
+import cors from "cors";
+
+//Create a new express application
+const app = express();
+
+//Tell express we want to use the morgan library
+app.use(morgan("dev"));
+//Tell express we want to use the cors library
+app.use(cors());
+//Tell express to parse JSON in the request body
+app.use(json());
+
+export { app };
